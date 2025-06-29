66575336bb5dd5d4349ddb4f135cbfe4c907dcc8
tpdkrt <tpodko@gmail.com>
2025-06-29 19:50:26 +0200

    deleted the main contents and replaced with app

--- sandbox/src/main.jsx
+++ sandbox/src/main.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RetroButton from "./components/RetroButton";
-import FlatRetroButton from "./components/FlatRetroButton";
-import Window from "./components/Window";
 import "./styles/main.css";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RetroButton content={"button text"}></RetroButton>
-    <FlatRetroButton content={"another button"} />
-    <button>not included button</button>
-    <Window content={<h1>Hello this is a window</h1>} />
+    <App></App>
   </React.StrictMode>
 );
