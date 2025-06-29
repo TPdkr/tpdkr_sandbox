ee1de7059d39a71e5d2c2ce40c382bc4cf2480f7
tpdkrt <tpodko@gmail.com>
2025-06-29 22:41:58 +0200

    created navbar

--- sandbox/src/App.jsx
+++ sandbox/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>This is an app</h1>
     </div>
   );
