8f8bb7bf8d6fbdbe93257e0bfce5274ed7ddfe2c
tpdkrt <tpodko@gmail.com>
2025-06-30 16:58:22 +0200

    ADDED ROUTING

--- sandbox/src/App.jsx
+++ sandbox/src/App.jsx
@@ -1,12 +1,36 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <h1>This is an app</h1>
-      <p>This is paragraph text</p>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <h1>Main page</h1>
+                <h1>This is an app</h1>
+                <p>This is paragraph text</p>
+              </div>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <div>
+                <h1>
+                  No page found<Link to="/">Home</Link>
+                </h1>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
