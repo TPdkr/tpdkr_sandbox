c792a1e54d8d9ca543ca456d8d50fbc29905dd3e
tpdkrt <tpodko@gmail.com>
2025-07-06 16:35:14 +0200

    fixed bug

--- sandbox/src/components/Navbar.jsx
+++ sandbox/src/components/Navbar.jsx
@@ -5,23 +5,19 @@ import { useState } from "react";
 export default function Navbar() {
   const [isNavVisible, setIsNavVisible] = useState(false);
   return (
-    <div>
+    <>
       <button
-        id="open-sidebar-button"
+        className={styles.OpenSidebarButton}
         aria-label="open sidebar"
-        aria-expanded="false"
         aria-controls="navbar"
-        onClick={() => {
-          setIsNavVisible(true);
-          console.log("opening the thing");
-        }}
+        onClick={() => setIsNavVisible(true)}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           height="40px"
           viewBox="0 -960 960 960"
           width="40px"
-          fill="#c9c9c9"
+          fill="#000000"
         >
           <path d="M165.13-254.62q-10.68 0-17.9-7.26-7.23-7.26-7.23-18t7.23-17.86q7.22-7.13 17.9-7.13h629.74q10.68 0 17.9 7.26 7.23 7.26 7.23 18t-7.23 17.87q-7.22 7.12-17.9 7.12H165.13Zm0-200.25q-10.68 0-17.9-7.27-7.23-7.26-7.23-17.99 0-10.74 7.23-17.87 7.22-7.13 17.9-7.13h629.74q10.68 0 17.9 7.27 7.23 7.26 7.23 17.99 0 10.74-7.23 17.87-7.22 7.13-17.9 7.13H165.13Zm0-200.26q-10.68 0-17.9-7.26-7.23-7.26-7.23-18t7.23-17.87q7.22-7.12 17.9-7.12h629.74q10.68 0 17.9 7.26 7.23 7.26 7.23 18t-7.23 17.86q-7.22 7.13-17.9 7.13H165.13Z" />
         </svg>
@@ -44,7 +40,7 @@ export default function Navbar() {
                 height="40px"
                 viewBox="0 -960 960 960"
                 width="40px"
-                fill="#c9c9c9"
+                fill="#000000"
               >
                 <path d="m480-444.62-209.69 209.7q-7.23 7.23-17.5 7.42-10.27.19-17.89-7.42-7.61-7.62-7.61-17.7 0-10.07 7.61-17.69L444.62-480l-209.7-209.69q-7.23-7.23-7.42-17.5-.19-10.27 7.42-17.89 7.62-7.61 17.7-7.61 10.07 0 17.69 7.61L480-515.38l209.69-209.7q7.23-7.23 17.5-7.42 10.27-.19 17.89 7.42 7.61 7.62 7.61 17.7 0 10.07-7.61 17.69L515.38-480l209.7 209.69q7.23 7.23 7.42 17.5.19 10.27-7.42 17.89-7.62 7.61-17.7 7.61-10.07 0-17.69-7.61L480-444.62Z" />
               </svg>
@@ -68,6 +64,6 @@ export default function Navbar() {
         onClick={() => setIsNavVisible(false)}
         aria-hidden="true"
       ></div>
-    </div>
+    </>
   );
 }
