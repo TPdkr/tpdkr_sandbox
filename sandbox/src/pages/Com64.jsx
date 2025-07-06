41e74093eaa96040955149b2c6984be99a09c474
tpdkrt <tpodko@gmail.com>
2025-07-06 16:38:00 +0200

    no more error logs on the page

--- sandbox/src/pages/Com64.jsx
+++ sandbox/src/pages/Com64.jsx
@@ -5,55 +5,59 @@ export default function Com64() {
     <div>
       <h1>These are inspired by commodore 64</h1>
       <table>
-        <tr>
-          <ColorCell color="hsl(197, 100%, 93%)">light pallte</ColorCell>
-          <ColorCell color="hsl(109, 50%, 90%)"></ColorCell>
-          <ColorCell color="hsl(47, 98%, 89%)"></ColorCell>
-          <ColorCell color="hsl(21, 96%, 94%)"></ColorCell>
-          <ColorCell color="hsl(9, 100%, 93%)"></ColorCell>
-          <ColorCell color="hsl(29, 14%, 67%)"></ColorCell>
-        </tr>
-        <tr>
-          <ColorCell color="hsl(197, 100%, 83%)"></ColorCell>
-          <ColorCell color="hsl(109, 50%, 80%)"></ColorCell>
-          <ColorCell color="hsl(47, 98%, 79%)"></ColorCell>
-          <ColorCell color="hsl(21, 96%, 84%)"></ColorCell>
-          <ColorCell color="hsl(9, 100%, 83%)"></ColorCell>
-          <ColorCell color="hsl(29, 14%, 57%)"></ColorCell>
-        </tr>
-        <tr>
-          <ColorCell color="hsl(197, 100%, 73%)"></ColorCell>
-          <ColorCell color="hsl(109, 50%, 70%)"></ColorCell>
-          <ColorCell color="hsl(47, 98%, 69%)"></ColorCell>
-          <ColorCell color="hsl(21, 96%, 74%)"></ColorCell>
-          <ColorCell color="hsl(9, 100%, 73%)"></ColorCell>
-          <ColorCell color="hsl(29, 14%, 47%)"></ColorCell>
-        </tr>
-        <tr>This is the base color pallete below</tr>
-        <tr>
-          <ColorCell color="hsl(197, 100%, 63%)">this is the og</ColorCell>
-          <ColorCell color="hsl(109, 50%, 60%)"></ColorCell>
-          <ColorCell color="hsl(47, 98%, 59%)"></ColorCell>
-          <ColorCell color="hsl(21, 96%, 64%)"></ColorCell>
-          <ColorCell color="hsl(9, 100%, 63%)"></ColorCell>
-          <ColorCell color="hsl(29, 14%, 37%)"></ColorCell>
-        </tr>
-        <tr>
-          <ColorCell color="hsl(197, 100%, 53%)"></ColorCell>
-          <ColorCell color="hsl(109, 50%, 50%)"></ColorCell>
-          <ColorCell color="hsl(47, 98%, 49%)"></ColorCell>
-          <ColorCell color="hsl(21, 96%, 54%)"></ColorCell>
-          <ColorCell color="hsl(9, 100%, 53%)"></ColorCell>
-          <ColorCell color="hsl(29, 14%, 27%)"></ColorCell>
-        </tr>
-        <tr>
-          <ColorCell color="hsl(197, 100%, 43%)"></ColorCell>
-          <ColorCell color="hsl(109, 50%, 40%)"></ColorCell>
-          <ColorCell color="hsl(47, 98%, 39%)"></ColorCell>
-          <ColorCell color="hsl(21, 96%, 44%)"></ColorCell>
-          <ColorCell color="hsl(9, 100%, 43%)"></ColorCell>
-          <ColorCell color="hsl(29, 14%, 17%)"></ColorCell>
-        </tr>
+        <tbody>
+          <tr>
+            <ColorCell color="hsl(197, 100%, 93%)">light pallte</ColorCell>
+            <ColorCell color="hsl(109, 50%, 90%)"></ColorCell>
+            <ColorCell color="hsl(47, 98%, 89%)"></ColorCell>
+            <ColorCell color="hsl(21, 96%, 94%)"></ColorCell>
+            <ColorCell color="hsl(9, 100%, 93%)"></ColorCell>
+            <ColorCell color="hsl(29, 14%, 67%)"></ColorCell>
+          </tr>
+          <tr>
+            <ColorCell color="hsl(197, 100%, 83%)"></ColorCell>
+            <ColorCell color="hsl(109, 50%, 80%)"></ColorCell>
+            <ColorCell color="hsl(47, 98%, 79%)"></ColorCell>
+            <ColorCell color="hsl(21, 96%, 84%)"></ColorCell>
+            <ColorCell color="hsl(9, 100%, 83%)"></ColorCell>
+            <ColorCell color="hsl(29, 14%, 57%)"></ColorCell>
+          </tr>
+          <tr>
+            <ColorCell color="hsl(197, 100%, 73%)"></ColorCell>
+            <ColorCell color="hsl(109, 50%, 70%)"></ColorCell>
+            <ColorCell color="hsl(47, 98%, 69%)"></ColorCell>
+            <ColorCell color="hsl(21, 96%, 74%)"></ColorCell>
+            <ColorCell color="hsl(9, 100%, 73%)"></ColorCell>
+            <ColorCell color="hsl(29, 14%, 47%)"></ColorCell>
+          </tr>
+          <tr>
+            <td>This is the base color pallete below</td>
+          </tr>
+          <tr>
+            <ColorCell color="hsl(197, 100%, 63%)">this is the og</ColorCell>
+            <ColorCell color="hsl(109, 50%, 60%)"></ColorCell>
+            <ColorCell color="hsl(47, 98%, 59%)"></ColorCell>
+            <ColorCell color="hsl(21, 96%, 64%)"></ColorCell>
+            <ColorCell color="hsl(9, 100%, 63%)"></ColorCell>
+            <ColorCell color="hsl(29, 14%, 37%)"></ColorCell>
+          </tr>
+          <tr>
+            <ColorCell color="hsl(197, 100%, 53%)"></ColorCell>
+            <ColorCell color="hsl(109, 50%, 50%)"></ColorCell>
+            <ColorCell color="hsl(47, 98%, 49%)"></ColorCell>
+            <ColorCell color="hsl(21, 96%, 54%)"></ColorCell>
+            <ColorCell color="hsl(9, 100%, 53%)"></ColorCell>
+            <ColorCell color="hsl(29, 14%, 27%)"></ColorCell>
+          </tr>
+          <tr>
+            <ColorCell color="hsl(197, 100%, 43%)"></ColorCell>
+            <ColorCell color="hsl(109, 50%, 40%)"></ColorCell>
+            <ColorCell color="hsl(47, 98%, 39%)"></ColorCell>
+            <ColorCell color="hsl(21, 96%, 44%)"></ColorCell>
+            <ColorCell color="hsl(9, 100%, 43%)"></ColorCell>
+            <ColorCell color="hsl(29, 14%, 17%)"></ColorCell>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
