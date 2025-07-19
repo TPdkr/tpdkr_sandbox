b1ea68e0646aa2257f30d8cd667c1e558caa2102
tpdkrt <tpodko@gmail.com>
2025-07-19 08:54:47 +0200

    window

--- sandbox/src/components/Window.jsx
+++ sandbox/src/components/Window.jsx
@@ -1,10 +1,10 @@
 import styles from "./Window.module.css";
 
-export default function Window(props) {
+export default function Window({ children }) {
   return (
     <div className={styles.Window}>
       <div className={styles.WindowBar}></div>
-      {props.content}
+      {children}
     </div>
   );
 }
--- sandbox/src/pages/Windows.jsx
+++ sandbox/src/pages/Windows.jsx
@@ -1,7 +1,30 @@
+import Window from "../components/Window";
+
 export default function Windows() {
   return (
     <>
       <h3>this is the windows page</h3>
+      <Window>
+        This text is contained in the windows Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus
+        animo, cum corpore dolemus, fieri tamen permagna accessio potest, si
+        aliquod aeternum et infinitum impendere malum nobis opinemur. Quod idem
+        licet transferre in voluptatem, ut postea variari voluptas distinguique
+        possit, augeri amplificarique non possit. At etiam. Locus; sed ita
+        sentio et saepe disserui, Latinam linguam non modo quid nobis
+        probaretur, sed etiam quid a singulis philosophiae disciplinis
+        diceretur, persecuti sumus. Ut autem a facillimis ordiamur, prima veniat
+        in medium Epicuri ratio, quae plerisque notissima est. Quam a nobis sic
+        intelleges eitam, ut ab ea nullo modo probantur, tamen Democritum
+        laudatum a ceteris ab hoc, qui eum unum. Assiduitas nec vigiliae nec ea
+        ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae
+        disciplinam iuvaret. An ille tempus aut in armatum hostem impetum
+        fecisse aut in poetis evolvendis, ut ego et Triarius te hortatore
+        facimus, consumeret, in quibus sequitur Democritum, non fere labitur.
+        Quamquam utriusque cum multa non probo, tum illud in primis, quod, cum
+        in rerum natura duo.
+      </Window>
     </>
   );
 }
